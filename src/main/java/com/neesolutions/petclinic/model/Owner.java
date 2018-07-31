f8bce7039a1fe17891861aa066da508b1a8d611e
Matt Nee <mattdnee@gmail.com>
2018-07-30 20:47:59 -0400

    adding POJOs. Closes #2.

---
+++ src/main/java/com/neesolutions/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.neesolutions.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/neesolutions/petclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.neesolutions.petclinic.model;
+
+public class Vet extends Person {
+
+}
