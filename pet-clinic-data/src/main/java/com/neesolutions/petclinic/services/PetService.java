5c822c2d8a912f27cb06b43f54c3cd69e4eae818
Matt Nee <mattdnee@gmail.com>
2018-08-18 20:46:59 -0400

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/com/neesolutions/petclinic/services/PetService.java
@@ -0,0 +1,11 @@
+package com.neesolutions.petclinic.services;
+
+import com.neesolutions.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/neesolutions/petclinic/services/Vet.java
@@ -0,0 +1,9 @@
+package com.neesolutions.petclinic.services;
+
+import java.util.Set;
+
+public interface Vet {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
