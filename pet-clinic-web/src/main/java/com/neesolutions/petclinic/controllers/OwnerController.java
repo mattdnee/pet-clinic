9ec370b2ed4855074a21b20e6685416652fc21ca
Matt Nee <mattdnee@gmail.com>
2018-09-01 13:47:39 -0400

    Add owners list and update owner controller, closes #16

--- pet-clinic-web/src/main/java/com/neesolutions/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/neesolutions/petclinic/controllers/OwnerController.java
@@ -1,13 +1,23 @@
 package com.neesolutions.petclinic.controllers;
 
+import com.neesolutions.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"",    "/", "index", "index.html"})
-    public String ListOwners(){
+    public String ListOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
