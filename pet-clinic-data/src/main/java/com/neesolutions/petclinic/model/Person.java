856464b080dcd5565ae499520587ea7de9ee5ad3
Matt Nee <mattdnee@gmail.com>
2018-09-03 15:14:25 -0400

    Add contact info to person, closes #28

--- pet-clinic-data/src/main/java/com/neesolutions/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/neesolutions/petclinic/model/Person.java
@@ -1,8 +1,12 @@
 package com.neesolutions.petclinic.model;
 
 public class Person extends BaseEntity{
+
     private String firstName;
     private String lastName;
+    private String address;
+    private String city;
+    private String telephone;
 
     public String getFirstName() {
         return firstName;
@@ -18,5 +22,29 @@ public class Person extends BaseEntity{
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 }
