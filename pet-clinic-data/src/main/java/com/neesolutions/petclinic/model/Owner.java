49d4db7ae51ef39467d136f5dea5844908343a44
Matt Nee <mattdnee@gmail.com>
2018-09-17 22:47:59 -0400

    Convert owner to a JPA Entity, closes #36

--- pet-clinic-data/src/main/java/com/neesolutions/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/neesolutions/petclinic/model/Owner.java
@@ -1,14 +1,23 @@
 package com.neesolutions.petclinic.model;
 
-import java.util.HashSet;
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets;
 
-    public Owner(){
-        pets = new HashSet<Pet>();
-    }
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "telephone")
+    private String telephone;
 
     public Set<Pet> getPets() {
         return pets;
@@ -16,5 +25,29 @@ public class Owner extends Person {
 
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 }
--- pet-clinic-data/src/main/java/com/neesolutions/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/neesolutions/petclinic/model/Person.java
@@ -1,12 +1,16 @@
 package com.neesolutions.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity{
 
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
-    private String address;
-    private String city;
-    private String telephone;
 
     public String getFirstName() {
         return firstName;
@@ -22,29 +26,5 @@ public class Person extends BaseEntity{
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
     }
 }
