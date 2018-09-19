205420283376b7018fb9af92782a171a7986f44c
Matt Nee <mattdnee@gmail.com>
2018-09-18 23:01:28 -0400

    Add Visit Service Add Visit Map Service implementation closes #46

---
+++ pet-clinic-data/src/main/java/com/neesolutions/petclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.neesolutions.petclinic.services;
+
+import com.neesolutions.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/neesolutions/petclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,45 @@
+package com.neesolutions.petclinic.services.map;
+
+import com.neesolutions.petclinic.model.Visit;
+import com.neesolutions.petclinic.services.PetService;
+import com.neesolutions.petclinic.services.VisitService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+    private final PetService petService;
+
+    public VisitServiceMap(@Autowired PetService petService) {
+        this.petService = petService;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet().getOwner() == null || visit.getPet().getOwner().getId() == null ||
+                visit.getPet() == null  ||visit.getPet().getId() == null)
+            throw new RuntimeException("Invalid Visit");
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
