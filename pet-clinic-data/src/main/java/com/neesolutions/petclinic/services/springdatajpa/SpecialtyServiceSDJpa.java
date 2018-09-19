bfbddb2e88769b4ccb86c979b7d54b3e798d8579
Matt Nee <mattdnee@gmail.com>
2018-09-18 22:16:54 -0400

    Add Specialty Spring Data JPA Repository, closes #44

---
+++ pet-clinic-data/src/main/java/com/neesolutions/petclinic/services/springdatajpa/SpecialtyServiceSDJpa.java
@@ -0,0 +1,49 @@
+package com.neesolutions.petclinic.services.springdatajpa;
+
+import com.neesolutions.petclinic.model.Specialty;
+import com.neesolutions.petclinic.repositories.SpecialtyRepository;
+import com.neesolutions.petclinic.services.SpecialtyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialtyServiceSDJpa implements SpecialtyService {
+
+    private SpecialtyRepository specialtyRepository;
+
+    public SpecialtyServiceSDJpa(@Autowired SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties = new HashSet<Specialty>();
+        specialtyRepository.findAll().forEach(specialties::add);
+        return specialties;
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
