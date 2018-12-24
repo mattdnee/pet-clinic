b0d7eba348f9a24ddf7bfe98c1d2ca0710c30519
Matt Nee <mattdnee@gmail.com>
2018-12-23 21:52:39 -0500

    Add find owner closes #54

--- pet-clinic-data/src/main/java/com/neesolutions/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/neesolutions/petclinic/repositories/OwnerRepository.java
@@ -3,6 +3,9 @@ package com.neesolutions.petclinic.repositories;
 import com.neesolutions.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/com/neesolutions/petclinic/services/springdatajpa/OwnerServiceSDJpa.java
+++ pet-clinic-data/src/main/java/com/neesolutions/petclinic/services/springdatajpa/OwnerServiceSDJpa.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -59,5 +60,10 @@ public class OwnerServiceSDJpa implements OwnerService {
     @Override
     public void deleteById(Long aLong) {
         ownerRepository.deleteById(aLong);
+    }
+
+    @Override
+    public List<Owner> findAllByLastNameLike(String lastName) {
+        return ownerRepository.findAllByLastNameLike(lastName);
     }
 }
